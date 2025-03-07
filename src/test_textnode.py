a9d3c381d0243b10d416d90c2a95098265390b72
bartzcarter <bartzcarter@gmail.com>
2025-03-07 09:53:45 -0700

    unit tests for equality operator

---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_type_not_equal(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_url_not_equal(self):
+        node = TextNode("This is a text node", TextType.BOLD, "http://google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "http://yahoo.com")
+        self.assertNotEqual(node, node2)
+
+    def test_text_not_equal(self):
+        node = TextNode("This is not text node", TextType.BOLD, "http://google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "http://google.com")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,10 +9,10 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
-        self.url = url if url else None
+        self.url = url
 
     def __eq__(self, other):
         if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
